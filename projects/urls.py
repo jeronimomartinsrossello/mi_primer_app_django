4730b5c896d528d83312e786c367f821e28c6b0e
jeronimomartinsrossello <jeronimormartins@gmail.com>
2025-12-02 00:56:50 +0100

    Fix: stop tracking ignored files

---
+++ projects/urls.py
@@ -0,0 +1,9 @@
+from rest_framework import routers
+from .api import ProjectViewSet 
+
+router = routers.DefaultRouter()
+
+router.register('api/projects', ProjectViewSet, 'project')
+
+urlpatterns = router.urls
+
